239ed654358f2d7a9ae9e40c2608f82a68b86cb4
Roger Zaugg <roger.zaugg@sbl.ch>
2025-03-24 10:10:03 +0100

    refactor: removed Factory and added a simple method in the facade for generating data object

--- src/logic/StudentFacade.java
+++ src/logic/StudentFacade.java
@@ -2,21 +2,39 @@ package logic;
 
 import java.util.ArrayList;
 
-import data.Factory;
-import data.Storage;
-import data.StudentDAO;
+import data.*;
 import entity.Student;
 
 public class StudentFacade {
 
-	private StudentDAO dao = Factory.getDAO(Storage.XML);
+	private StudentDAO dao;
 
+	public StudentFacade() {
+		this.setDAO(Storage.MEMORY);
+	}
 	public ArrayList<Student> getAllStudents()  {
 		return this.dao.getStudents();
 	}
-	
-	public void addStudent(Student s) {
-		this.dao.saveStudent(s);
+
+	public void addStudent(Student student) {
+		this.dao.saveStudent(student);
 	}
-	
+
+	private void setDAO(Storage storage) {
+		switch (storage) {
+			case XML:
+				dao = new StudentDAOXML();
+				break;
+
+			case DB:
+				dao = new StudentDAODB();
+				break;
+
+			default:
+				dao = new StudentDAOMemory();
+				break;
+		}
+	}
+
+
 }
